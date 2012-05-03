3b80c9e03b040e34847d3ade6d16c307790f506e
Jeremy Wilmot <j1wilmot@gmail.com>
2012-05-03 00:28:17 -0400

    Finsihed tests and Solve

--- GoogleCodeJam/src/qualification2012/speaking_in_tongues/Solve.java
+++ GoogleCodeJam/src/qualification2012/speaking_in_tongues/Solve.java
@@ -1,8 +1,65 @@
 package qualification2012.speaking_in_tongues;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+
 public class Solve {
 
-	public static void main(String... args) {
-		System.out.print(System.getProperty("user.dir"));
+	private static Map<Character, Character> translationMap;
+
+	protected static void init() {
+		String outputString = "our language is impossible to understand\n" +
+				"there are twenty six factorial possibilities\n" +
+				"so it is okay if you want to just give up";
+
+		String inputString = "ejp mysljylc kd kxveddknmc re jsicpdrysi\n" +
+				"rbcpc ypc rtcsra dkh wyfrepkym veddknkmkrkcd\n" +
+				"de kr kd eoya kw aej tysr re ujdr lkgc jv";
+
+		translationMap = createTranslationMap(outputString, inputString);
+	}
+
+	public static void main(String... args) throws IOException {
+		init();
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		String line = reader.readLine();
+		int numLines = Integer.parseInt(line);
+		for (int i = 0; i < numLines; i++) {
+			line = reader.readLine();
+			System.out.print("Case #" + (i + 1) + ": ");
+			for (char c : line.toCharArray()) {
+				Character translated = translationMap.get(c);
+				if (translated ==  null)
+					System.err.print(c);
+				else
+					System.out.write(translated);
+			}
+			System.out.println();
+		}
+	}
+	
+	public static Map<Character, Character> getTranslationMap() {
+		return translationMap;
+	}
+
+	private static Map<Character, Character> createTranslationMap(
+			String outputString, String inputString) {
+		Map<Character, Character> translate = new HashMap<Character, Character>();
+
+		char[] output = outputString.toCharArray();
+		char[] input = inputString.toCharArray();
+		
+		for (int i = 0; i < outputString.length(); i++) {
+			translate.put(input[i], output[i]);
+		}
+
+		// Mappings determined outside of sample paragraph
+		translate.put('a', 'y');
+		translate.put('z', 'q');
+		translate.put('q', 'z');
+		return translate;
 	}
 }
--- GoogleCodeJam/src/qualification2012/speaking_in_tongues/SolveTest.java
+++ GoogleCodeJam/src/qualification2012/speaking_in_tongues/SolveTest.java
@@ -2,43 +2,75 @@ package qualification2012.speaking_in_tongues;
 
 import static org.junit.Assert.*;
 
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.ByteArrayOutputStream;
-import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintStream;
-import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 public class SolveTest {
+	private String path = "src/qualification2012/speaking_in_tongues/";
+	private String expectedOutputFilePath = path + "testOutput.txt";
+	private String inputFilePath = path + "testInput.txt";
 	
-	private String expectedOutputFilePath = "src/qualification2012/speaking_in_tongues/testOutput.txt";
+	private Map<Character, Character> translation;
+	private List<Character> letters;
+	
+	@Before
+	public void getTranslationMap() {
+		Solve.init();
+		char[] alphabet = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+		letters = new ArrayList<Character>(alphabet.length);
+		for (char c : alphabet) {
+			letters.add(c);
+		}
+		translation = Solve.getTranslationMap();
+	}
 	
 	@Test
-	public void test() throws IOException {
-		// Capture stdout from solve
+	public void verifySampleInput() throws IOException {
+		// Capture stdout of Solve
 		ByteArrayOutputStream baos = new ByteArrayOutputStream();
 		System.setOut(new PrintStream(baos));
-		StringBuilder expectedOutput = new StringBuilder();
+		
+		// Set up stdin for Solve
+		System.setIn(new FileInputStream(inputFilePath));
+		Solve.main();
+		
+		// Gather expected text
 		BufferedReader reader = new BufferedReader(new FileReader(expectedOutputFilePath));
+		StringBuilder expectedOutput = new StringBuilder();
 		String line;
 		while ((line = reader.readLine()) != null) {
 			expectedOutput.append(line + "\n");
 		}
-		
-		
-		Solve.main("");
-		
-		
 		assertEquals(expectedOutput.toString(), baos.toString());
 	}
-
+	
+	@Test
+	public void allLettersAreMapped() {
+		List<Character> missingLetters = new ArrayList<Character>();
+		for (char c : letters) {
+			if (!translation.containsKey(c))
+				missingLetters.add(c);
+		}
+		assertTrue("Translation map should contain all letters as keys but is missing " + missingLetters, missingLetters.isEmpty());
+	}
+	
+	@Test
+	public void allLettersAreMappedTo() {
+		List<Character> missingValues = new ArrayList<Character>();
+		for (char c : letters) {
+			if (!translation.containsValue(c))
+				missingValues.add(c);
+		}
+		assertTrue("Translation map should contain all letters as values but is missing " + missingValues, missingValues.isEmpty());
+	}
 }
